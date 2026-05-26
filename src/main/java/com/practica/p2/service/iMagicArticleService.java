3d1a57b9d44c4e7e8255d57ecf2f64d681123d61
jlmilan17 <00144723@uca.edu.sv>
2026-05-25 20:10:06 -0600

    Definición de los metodos de magicArticles

--- src/main/java/com/practica/p2/service/iMagicArticleService.java
+++ src/main/java/com/practica/p2/service/iMagicArticleService.java
@@ -1,10 +1,19 @@
 package com.practica.p2.service;
 
+import com.practica.p2.domain.MagicProvider;
+import com.practica.p2.domain.dtos.MagicArticleRequestDTO;
+import com.practica.p2.domain.dtos.MagicArticleResponseDTO;
 import com.practica.p2.domain.dtos.MagicProviderRequestDTO;
 import com.practica.p2.domain.dtos.MagicProviderResponseDTO;
+import com.practica.p2.domain.enums.Type;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 public interface iMagicArticleService {
-
+    public MagicArticleResponseDTO create(MagicArticleRequestDTO requestDTO);
+    public MagicArticleResponseDTO getArtefactsById(UUID uuid);
+    public MagicArticleRequestDTO getArtefactsByCategoryOrProviderOrMaxPrice(Type category, MagicProviderRequestDTO providerRequestDTO, BigDecimal price);
+    public MagicArticleResponseDTO updateProvider(UUID uuid, MagicArticleRequestDTO requestDTO);
+    public void deleteProvider(UUID uuid);
 }
--- src/main/java/com/practica/p2/service/serviceImp/MagicArticleServiceImp.java
+++ src/main/java/com/practica/p2/service/serviceImp/MagicArticleServiceImp.java
@@ -1,7 +1,38 @@
 package com.practica.p2.service.serviceImp;
 
+import com.practica.p2.domain.dtos.MagicArticleRequestDTO;
+import com.practica.p2.domain.dtos.MagicArticleResponseDTO;
+import com.practica.p2.domain.dtos.MagicProviderRequestDTO;
+import com.practica.p2.domain.enums.Type;
 import com.practica.p2.service.iMagicArticleService;
+
+import java.math.BigDecimal;
+import java.util.UUID;
 
 public class MagicArticleServiceImp implements iMagicArticleService {
 
+    @Override
+    public MagicArticleResponseDTO create(MagicArticleRequestDTO requestDTO) {
+        return null;
+    }
+
+    @Override
+    public MagicArticleResponseDTO getArtefactsById(UUID uuid) {
+        return null;
+    }
+
+    @Override
+    public MagicArticleRequestDTO getArtefactsByCategoryOrProviderOrMaxPrice(Type category, MagicProviderRequestDTO providerRequestDTO, BigDecimal price) {
+        return null;
+    }
+
+    @Override
+    public MagicArticleResponseDTO updateProvider(UUID uuid, MagicArticleRequestDTO requestDTO) {
+        return null;
+    }
+
+    @Override
+    public void deleteProvider(UUID uuid) {
+
+    }
 }
